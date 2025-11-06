8adfa55627fcc09654b97b04e19e16a53c624ce2
OutstandingVick <scottdodo2015@outlook.com>
2025-11-06 15:53:34 +0100

    add a new home page section

--- src/Home.js
+++ src/Home.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <article className='dark:bg-dain dark:text-side bg-main text-support mt-7 md:mt-14'>
-      <div className='font-lato space-y-3 md:space-y-6 text-2xl md:text-6xl'>
+    <article className='dark:bg-dain dark:text-side bg-main text-support mt-7 md:mt-14 overflow-hidden font-lato'>
+      <div className='font-lato space-y-3 md:space-y-5 text-2xl md:text-6xl'>
         <div className='space-y-3 md:space-y-6 left'>
           <h2>
-            I Make Written{' '}
+            I Make Written
             <span className=' dark:text-main text-side'>Web3</span> Content
           </h2>
           <h2>
-            That Informs,{' '}
+            That Informs,
             <span className='dark:text-main text-side'>Compels</span>,
           </h2>
           <h2>
@@ -43,12 +43,22 @@ export default function Home() {
         </div>
       </div>
       <br />
-      <div className='flex justify-center font-lato'>
+      <div className='flex justify-center'>
         <button className='text-xl py-4 px-6 rounded-full border-2 dark:text-side dark:border-side text-support border-support transition-transform duration-200 hover:scale-105'>
           Connect With Me!
         </button>
       </div>
       <br />
+      <div className='flex flex-row px-12 justify-between '>
+        <h1 className='text-6xl'>Featured Projects</h1>
+        <div className=''>
+          <p>
+            A curated set of projects from research papers to articles to mix of
+            both
+          </p>
+          <p>built with creativity, readability, and engagement in mind.</p>
+        </div>
+      </div>
     </article>
   );
 }
